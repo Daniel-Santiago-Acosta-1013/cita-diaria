fdc07a7655762ced3c64471b2771f2c86663d007
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-05-26 18:39:10 -0500

    [add] adgrando animacion de trancision

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -12,12 +12,27 @@ const getQuotes = async () => {
 };
 
 const updateQuote = () => {
-  document.getElementById('quote-text')!.textContent = quotes[currentQuoteIndex].text;
-  document.getElementById('quote-author')!.textContent = '- ' + quotes[currentQuoteIndex].author;
+  // Obtén las referencias a los elementos que contienen el texto de la cita y el autor
+  const quoteTextElement = document.getElementById('quote-text')!;
+  const quoteAuthorElement = document.getElementById('quote-author')!;
+
+  // Agrega la clase "quote-change" para iniciar la animación
+  quoteTextElement.classList.add('quote-change');
+  quoteAuthorElement.classList.add('quote-change');
+
+  // Después de 0.5s (la duración de la animación), actualiza el texto y quita la clase "quote-change"
+  setTimeout(() => {
+    quoteTextElement.textContent = quotes[currentQuoteIndex].text;
+    quoteAuthorElement.textContent = '- ' + quotes[currentQuoteIndex].author;
+    
+    quoteTextElement.classList.remove('quote-change');
+    quoteAuthorElement.classList.remove('quote-change');
+  }, 500);
 };
 
 document.getElementById('new-quote-button')!.addEventListener('click', async () => {
   await getQuotes();
 });
 
+// Llama a getQuotes al cargar la página para iniciar con una cita
 getQuotes();
